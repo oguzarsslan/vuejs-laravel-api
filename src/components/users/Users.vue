5c103d949dd7f85b9d1d2e8e05f1b31796496d9e
oguzarsslan <oguz139@gmail.com>
2021-12-05 15:25:16 +0300

    friends

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -21,9 +21,52 @@
                 <td>
                   <button class="btn-xs btn-danger" @click="deleteUser(item.id)">delete</button>
                 </td>
-                <td>
+                <td v-if="item.status === 'accepted'">
+                  <button class="btn-xs btn-success"
+                          :class="{'btn-danger' : cancel}"
+                          @mouseover="mouseOver(item.id)"
+                          @mouseleave="mouseOver(item.id)"
+                          @click="removeFriend(item.id)"
+                  >
+                    {{ cancel ? 'Remove' : 'Friend' }}
+                  </button>
+                </td>
+                <td v-else-if="item.status === 'pending' && item.sender_id === item.id">
+                  <button class="btn-xs btn-success"
+                          @click="addFriends(item.id)"
+                  >
+                    Accept
+                  </button>
+                  <button class="btn-xs btn-danger"
+                          @click="removeFriend(item.id)"
+                  >
+                    Cancel
+                  </button>
+                </td>
+                <td v-else-if="item.status === 'pending'">
+                  <button class="btn-xs btn-info"
+                          :class="{'btn-danger' : cancel}"
+                          @mouseover="mouseOver(item.id)"
+                          @mouseleave="mouseOver(item.id)"
+                          @click="removeFriend(item.id)"
+                  >
+                    {{ cancel ? 'Cancel' : 'Pending' }}
+                  </button>
+                </td>
+                <td v-else-if="item.status === 'blocked'">
+                  <button class="btn-xs"
+                          :class="{'btn-danger' : cancel, 'btn-warning' : !cancel}"
+                          @mouseover="mouseOver()"
+                          @mouseleave="mouseOver()"
+                          @click="unblocked(item.id)"
+                  >
+                    {{ cancel ? 'Unblocked' : 'Blocked' }}
+                  </button>
+                </td>
+                <td v-else>
                   <button class="btn-xs btn-success">Add Friends</button>
                 </td>
+                <td>{{ item.status }}</td>
               </tr>
               </tbody>
             </table>
@@ -41,7 +84,9 @@ import Profile from "../profile/Profile";
 export default {
   name: "Users",
   data() {
-    return {}
+    return {
+      cancel: false
+    }
   },
   components: {Profile},
   methods: {
@@ -52,6 +97,24 @@ export default {
       this.$store.dispatch('deleteUser', itemid).then(response => {
         this.getDataFromServer();
       })
+    },
+    removeFriend(itemid) {
+      this.$store.dispatch('removeFriend', itemid).then(response => {
+        this.getDataFromServer();
+      })
+    },
+    unblocked(itemid) {
+      this.$store.dispatch('unblocked', itemid).then(response => {
+        this.getDataFromServer();
+      })
+    },
+    addFriends(itemid) {
+      this.$store.dispatch('addFriends', itemid).then(response => {
+        this.getDataFromServer();
+      })
+    },
+    mouseOver() {
+      this.cancel = !this.cancel;
     }
   },
   computed: {
